af52b130a75b1931440c1ff9c0487ccc1beb7c0c
federico-paganini <perfedefer@gmail.com>
2025-10-10 22:54:02 -0300

    refactor: Email body moved to own function. Send email decoupling from process_csv to create a background_task.

--- app/services/csv_invitation/csv_invitation.py
+++ app/services/csv_invitation/csv_invitation.py
@@ -1,5 +1,5 @@
 import csv
-from typing import BinaryIO, List
+from typing import List
 
 from app.domain.invitation import Invitation
 from app.services.csv_invitation.exceptions import (
@@ -15,28 +15,24 @@ class CSVInvitationProcessor:
     def __init__(self, email_service=resend_email_service):
         self.email_service = email_service
 
-    def process_csv(self, file: BinaryIO):
+    def process_csv(self, file_contents: bytes):
         """Orquesta todo el flujo de validación, creación, guardado y envío"""
-        graduates = self._validate_csv(file)
+        graduates = self._validate_csv(file_contents)
         invitations = self._generate_invitations(graduates)
-        self._send_invitations(invitations)
+        return invitations
 
     # Private methods (Modularization).
-    def _validate_csv(self, file: BinaryIO):
+    def _validate_csv(self, file_contents: bytes):
         try:
-            file.seek(0)
-            reader = csv.DictReader(file.read().decode("utf-8").splitlines())
+            content = file_contents.decode("utf-8")
+            reader = csv.DictReader(content.splitlines())
         except UnicodeDecodeError:
             raise InvalidCSVException("Not valid CSV")
 
         if not reader.fieldnames:
             raise InvalidCSVException("CSV is empty or missing header")
 
-        missing = [
-            col
-            for col in self.REQUIRED_COLUMNS
-            if col not in reader.fieldnames
-        ]
+        missing = [col for col in self.REQUIRED_COLUMNS if col not in reader.fieldnames]
         if missing:
             raise MissingColumnsException(missing)
 
@@ -53,58 +49,60 @@ class CSVInvitationProcessor:
             invitations.append(invitation)
         return invitations
 
-    def _send_invitations(self, invitations: List[Invitation]):
+    def send_invitations(self, invitations: List[Invitation]):
         for invitation in invitations:
-            body = f"""
-            <div style="
-                width: 100%;
-                min-height: 300px;
-                max-width: 1200px;
-                margin: 0 auto;
-                background-color: #ffffff;
-                font-family: Arial, sans-serif;
-            ">
-                <section id="header" style="
-                        background: linear-gradient(to right, #cb5554, #e9a075);
-                        padding: 20px;
-                        color: rgb(234, 245, 255);
-                        font-family: Arial, sans-serif;
-                    ">
-                <h1 style="margin: 0; margin-left: 20px;">Welcome to Spotly!</h1>
-                </section>
-
-                <section id="body" style="
-                    padding: 20px;
-                    font-family: Arial, sans-serif;
-                ">
-                    <p>Hello! {invitation.full_name}</p>
-                    <p>To log in and start using Spotly please use this link: <span style="color: red">http://spotly.work/sign-up/{invitation.token}</span></p>
-                </section>
-                
-                <section id="footer" style="
-                    background: linear-gradient(to right, #cb5554, #e9a075);
-                    padding: 20px;
-                    color: rgb(234, 245, 255);
-                    font-family: Arial, sans-serif;
-                    text-align: center;
-                    font-size: 0.9em;
-                    margin-top: 20px;
-                ">
-                    <p style="margin: 0.5em 0;">Thanks for using Spotly!</p>
-                    <p style="margin: 0.5em 0; font-size: 0.8em;">
-                        If you have any questions, contact a
-                        <a href="https://spotly.work/contact" style="color: rgb(234, 245, 255); text-decoration: underline;">Spotly Supervisor</a>
-                        agent
-                    </p>
-                    <p style="margin: 0.5em 0; font-size: 0.7em; opacity: 0.8;">
-                        2025 Spotly. Pending Rights Reservation.
-                    </p>
-                </section>
-            </div>
-            """
-
+            body = self._build_email_body(invitation)
             self.email_service.send_email(
                 invitation.email,
                 "Spotly app invitation from Holberton",
                 body,
             )
+
+    def _build_email_body(self, invitation: Invitation) -> str:
+        return f"""
+        <div style="
+            width: 100%;
+            min-height: 300px;
+            max-width: 1200px;
+            margin: 0 auto;
+            background-color: #ffffff;
+            font-family: Arial, sans-serif;
+        ">
+            <section id="header" style="
+                    background: linear-gradient(to right, #cb5554, #e9a075);
+                    padding: 20px;
+                    color: rgb(234, 245, 255);
+                    font-family: Arial, sans-serif;
+                ">
+            <h1 style="margin: 0; margin-left: 20px;">Welcome to Spotly!</h1>
+            </section>
+
+            <section id="body" style="
+                padding: 20px;
+                font-family: Arial, sans-serif;
+            ">
+                <h2>Hello! {invitation.full_name}</h2>
+                <h4>To log in and start using Spotly please use this link: <span style="color: red">http://spotly.work/sign-up/invite?token={invitation.token}</span></h4>
+            </section>
+            
+            <section id="footer" style="
+                background: linear-gradient(to right, #cb5554, #e9a075);
+                padding: 20px;
+                color: rgb(234, 245, 255);
+                font-family: Arial, sans-serif;
+                text-align: center;
+                font-size: 0.9em;
+                margin-top: 20px;
+            ">
+                <p style="margin: 0.5em 0;">Thanks for using Spotly!</p>
+                <p style="margin: 0.5em 0; font-size: 0.8em;">
+                    If you have any questions, contact a
+                    <a href="https://spotly.work/contact" style="color: rgb(234, 245, 255); text-decoration: underline;">Spotly Supervisor</a>
+                    agent
+                </p>
+                <p style="margin: 0.5em 0; font-size: 0.7em; opacity: 0.8;">
+                    2025 Spotly. Pending Rights Reservation.
+                </p>
+            </section>
+        </div>
+        """
