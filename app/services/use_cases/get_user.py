b539df4669e350bfba890907b4d71a828e34e50a
federico-paganini <perfedefer@gmail.com>
2025-10-15 21:43:00 -0300

    feat: Login and me basic logic add.

--- app/services/use_cases/get_user.py
+++ app/services/use_cases/get_user.py
@@ -1,36 +1,39 @@
 from jose import jwt, JWTError
+import logging
 
 from app.settings import settings
-from app.domain.models.invitation import Invitation
-from app.infrastructure.database.repositories.invitation_repository import (
-    InvitationRepository,
-)
+from app.domain.models.user import User
+from app.infrastructure.database.repositories.user_repository import UserRepository
 from app.services.exceptions.user_login_exceptions import UserNotRegistered
+
+logger = logging.getLogger(__name__)
 
 
 class GetUser:
     def __init__(
         self,
-        user_repo: InvitationRepository = None,
+        user_repo: UserRepository = None,
     ):
-        self.user_repo = user_repo or InvitationRepository()
+        self.user_repo = user_repo or UserRepository()
 
-    async def execute(self, token: str) -> str:
+    async def verify(self, token: str) -> User:
         try:
+            logger.info(f"Token: {token}")
             payload = jwt.decode(
                 token, settings.secret_key, algorithms=[settings.algorithm]
             )
             user_id: str = payload.get("sub")
+            logger.info(f"Payload: {payload}, User_id: {user_id}, Token: {token}")
 
             if user_id is None:
                 raise JWTError
-        except JWTError:
+        except JWTError as e:
+            logging.warning(f"Error: {e}")
             raise UserNotRegistered("Expired or invalid token")
 
-        # Buscar el usuario en la BD
         user = await self.user_repo.find_by_id(user_id)
 
         if user is None:
             raise UserNotRegistered("User not found")
 
-        return Invitation(**user)
+        return User(**user)
--- app/services/use_cases/user_login.py
+++ app/services/use_cases/user_login.py
@@ -1,34 +1,37 @@
 from datetime import datetime, timedelta
 from jose import jwt
+import logging
 
 from app.settings import settings
-from app.domain.models.invitation import Invitation
-from app.infrastructure.database.repositories.invitation_repository import (
-    InvitationRepository,
-)
+from app.domain.models.user import User
+from app.infrastructure.database.repositories.user_repository import UserRepository
 from app.services.exceptions.user_login_exceptions import UserNotRegistered
 
+logger = logging.getLogger(__name__)
 
-class AuthUser:
+
+class UserLogin:
     def __init__(
         self,
-        user_repo: InvitationRepository = None,
+        user_repo: UserRepository = None,
     ):
-        self.user_repo = user_repo or InvitationRepository()
+        self.user_repo = user_repo or UserRepository()
 
-    async def execute(self, email: str) -> str:
+    async def login(self, email: str) -> str:
         user = await self._verify_user(email)
+        logger.info(f"User model: {user.to_dict()}")
         expire = datetime.now() + timedelta(
             minutes=settings.access_token_expire_minutes
         )
         payload = {"sub": str(user.id), "exp": expire}
 
         token = jwt.encode(payload, settings.secret_key, algorithm=settings.algorithm)
-        return token
+        data = {"token": token, "role": user.role}
+        return data
 
-    async def _verify_user(self, email: str) -> Invitation:
+    async def _verify_user(self, email: str) -> User:
         user_data = await self.user_repo.find_by_email(email)
         if not user_data:
             raise UserNotRegistered("Invalid email")
-        user = Invitation(**user_data)
+        user = User(**user_data)
         return user
