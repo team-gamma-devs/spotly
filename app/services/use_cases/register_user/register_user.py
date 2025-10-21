3ab7e16646cf17cc6613f423209ab941676de31f
federico-paganini <perfedefer@gmail.com>
2025-10-21 16:09:14 -0300

    fix: avatar_img type str to UploadFile

--- app/services/use_cases/register_user/register_user.py
+++ app/services/use_cases/register_user/register_user.py
@@ -2,9 +2,13 @@ from fastapi import UploadFile
 import logging
 
 from app.services.use_cases.register_user.cv_processor import CVProcessor
-from app.services.use_cases.register_user.cv_info_processor import CVInfoProcessor
+from app.services.use_cases.register_user.cv_info_processor import (
+    CVInfoProcessor,
+)
 from app.services.use_cases.register_user.user_processor import UserProcessor
-from app.infrastructure.database.repositories.user_repository import UserRepository
+from app.infrastructure.database.repositories.user_repository import (
+    UserRepository,
+)
 from app.infrastructure.database.repositories.invitation_repository import (
     InvitationRepository,
 )
@@ -15,11 +19,11 @@ logger = logging.getLogger(__name__)
 class RegisterUser:
     def __init__(
         self,
-        user_repo: UserRepository = None,
-        invitation_repo: InvitationRepository = None,
-        cv_processor: CVProcessor = None,
-        cv_info_processor: CVInfoProcessor = None,
-        user_processor: UserProcessor = None,
+        user_repo: UserRepository | None = None,
+        invitation_repo: InvitationRepository | None = None,
+        cv_processor: CVProcessor | None = None,
+        cv_info_processor: CVInfoProcessor | None = None,
+        user_processor: UserProcessor | None = None,
     ):
         self.user_repo = user_repo or UserRepository()
         self.invitation_repo = invitation_repo or InvitationRepository()
@@ -31,14 +35,16 @@ class RegisterUser:
         self,
         personal_cv: UploadFile,
         linkedin_cv: UploadFile,
-        avatar_img: str,
-        github_username: str = None,
-        token: str = None,
+        avatar_img: UploadFile,
+        github_username: str | None = None,
+        token: str | None = None,
     ):
         logger.info(f"User registration process started with token: {token}")
         cv_info = await self.cv_processor.process_cvs(personal_cv, linkedin_cv)
         logger.info(f"User cv info successfully parsed: {cv_info}")
-        registered_user_id = await self.user_processor.process_user(cv_info, avatar_img)
+        registered_user_id = await self.user_processor.process_user(
+            cv_info, avatar_img
+        )
         logger.info(f"User registered succesfully (id): {registered_user_id}")
         cv_info_id = await self.cv_info_processor.process_cv_info(
             cv_info, registered_user_id, personal_cv
@@ -46,7 +52,9 @@ class RegisterUser:
         if github_username:
             pass
         update_user = await self.user_processor.update_user_external_data(
-            cv_info_id, user_id=registered_user_id, github_info_id=None
+            cv_info_id=cv_info_id,
+            user_id=registered_user_id,
+            github_info_id="",
         )
 
         if not update_user:
