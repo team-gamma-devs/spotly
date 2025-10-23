b6c08089461fa4ec494ae082b30a8f6b56f5a818
federico-paganini <perfedefer@gmail.com>
2025-10-23 08:15:25 -0300

    refacotor: Now CVInfo is embded in user, no need to save in db

--- app/services/use_cases/register_user/cv_info_processor.py
+++ app/services/use_cases/register_user/cv_info_processor.py
@@ -1,44 +1,44 @@
 from fastapi import UploadFile
-from typing import Dict, Any
+from typing import Dict, Any, List, Optional
+import logging
 
 from app.domain.models.cvinfo import CVInfo
-from app.infrastructure.database.repositories.cv_info_repository import (
-    CVInfoRepository,
-)
 from app.infrastructure.database.repositories.filters_repository import (
     FiltersRepository,
 )
+
+logger = logging.getLogger(__name__)
 
 
 class CVInfoProcessor:
     def __init__(
         self,
-        cv_info_repo: CVInfoRepository = None,
-        filters_repo: FiltersRepository = None,
+        filters_repo: Optional[FiltersRepository] = None,
         bucket_service=None,
     ):
-        self.cv_info_repo = cv_info_repo or CVInfoRepository()
         self.filters_repo = filters_repo or FiltersRepository()
         self.bucket_service = bucket_service
 
     async def process_cv_info(
-        self, cv_info: Dict[str, Any], user_id: str, cv_file: UploadFile
+        self, cv_info: Dict[str, Any], cv_file: UploadFile
     ) -> str:
         cv_file_url = await self._upload_cv(cv_file)
         cv_data = {
-            "graduated_id": user_id,
-            "personal_cv_url": "https://placeholder.com",
+            "personal_cv_url": cv_file_url,
             "skills": cv_info["skills"],
             "english_level": cv_info["english_level"],
             "works_in_it": cv_info["works_in_it"],
         }
+
         user_cv_data = CVInfo(**cv_data)
-        user_cv_data_id = await self._save_cv_data(user_cv_data)
-        if not user_cv_data_id:
-            raise Exception("Unexpected error saving user_data in DB")
+        logger.info(f"CV data to save: {user_cv_data.to_dict()}")
+
+        await self._update_filters(user_cv_data.skills)
+        return user_cv_data
 
     async def _upload_cv(self, cv_file: UploadFile) -> str:
-        pass
+        return "https://placeholder.com"
 
-    async def _save_cv_data(self, user_cv_data: CVInfo) -> str:
-        return self.cv_info_repo.create(user_cv_data.to_dict())
+    async def _update_filters(self, filters: List[str]):
+        await self.filters_repo.add_user_filters(filters)
+        logger.warning(f"Filters correctly added to list")
