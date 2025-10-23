4ada8c147ce62d7790dbf73a97cb7a160c622f24
federico-paganini <perfedefer@gmail.com>
2025-10-23 08:18:01 -0300

    feat: Verification for existing user and cv_info embding.

--- app/services/use_cases/register_user/user_processor.py
+++ app/services/use_cases/register_user/user_processor.py
@@ -1,8 +1,9 @@
 from fastapi import UploadFile
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 import logging
 
 from app.domain.models.user import User
+from app.domain.models.cvinfo import CVInfo
 from app.infrastructure.database.repositories.user_repository import UserRepository
 
 logger = logging.getLogger(__name__)
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 class UserProcessor:
     def __init__(
         self,
-        user_repo: UserRepository = None,
+        user_repo: Optional[UserRepository] = None,
         bucket_service=None,
     ):
         self.user_repo = user_repo or UserRepository()
@@ -20,10 +21,14 @@ class UserProcessor:
     async def process_user(
         self,
         cv_info: Dict[str, Any],
+        cv_info_data: CVInfo,
         avatar_img: UploadFile,
         email: str = "perfedefer@gmail.com",
         cohort: int = 26,
     ):
+        if await self.user_repo.user_email_exists(email):
+            raise Exception(f"User with email {email} already registered")
+
         avatar_img_url = await self._save_avatar_img(avatar_img)
         user_data = {
             "first_name": cv_info["first_name"],
@@ -31,7 +36,9 @@ class UserProcessor:
             "email": email,
             "avatar_url": avatar_img_url,
             "cohort": cohort,
+            "cv_data": cv_info_data,
         }
+
         new_user = User(**user_data)
         user_id = await self._save_user(new_user)
         return user_id
@@ -44,11 +51,9 @@ class UserProcessor:
         return user_id
 
     async def update_user_external_data(
-        self, user_id: str, cv_info_id: str, github_info_id: str = None
+        self, user_id: str, github_info_id: Optional[str] = None
     ) -> bool:
-        data = {
-            "cv_info_id": cv_info_id,
-        }
+        data = {}
         if github_info_id:
             data["github_info"] = github_info_id
         update_result = await self.user_repo.update(user_id, data)
