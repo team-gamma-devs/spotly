b8f74236ba74886ac16b083822d93f0a7ec9a96b
federico-paganini <perfedefer@gmail.com>
2025-10-23 08:06:34 -0300

    WIP: CVInfo needs to be embded in User.

--- app/domain/models/cvinfo.py
+++ app/domain/models/cvinfo.py
@@ -1,66 +1,71 @@
-from typing import List
+from typing import List, Optional, Literal
 from datetime import datetime
 
 from app.domain.models.bmodel import BModel
 
 
-class CVInfo(BModel):
+class CVInfo:
     def __init__(
         self,
-        graduated_id: str,
         personal_cv_url: str,
         skills: List[str],
         english_level: str,
         works_in_it: bool,
-        created_at: datetime | None = None,
-        updated_at: datetime | None = None,
-        id: str | None = None,
+        last_update: Optional[datetime] = None,
     ):
-        super().__init__(id, created_at, updated_at)
-        self.__graduated_id = BModel.validate_id(graduated_id, "graduated_id")
-        self.personal_cv_url = BModel.validate_url(
-            personal_cv_url, "personal_cv_url"
-        )
+        self.personal_cv_url = personal_cv_url
         self.skills = skills
         self.english_level = english_level
         self.works_in_it = works_in_it
+        self.last_update = last_update
 
-    @property
-    def graduated_id(self):
-        return self.__graduated_id
-
-    """
     @property
     def personal_cv_url(self):
         return self.__personal_cv_url
 
     @property
     def skills(self):
-        return self.__skills
+        return self._skills
 
     @property
     def english_level(self):
         return self.__english_level
 
-    @property
-    """
+    @personal_cv_url.setter
+    def personal_cv_url(self, value: str):
+        self.__personal_cv_url = BModel.validate_url(value, "personal_cv_url")
+
+    @english_level.setter
+    def english_level(self, value: Literal["basic", "intermediate", "advanced"]):
+        VALID_ENGLISH_LEVELS = ["basic", "intermediate", "advanced"]
+        if value not in VALID_ENGLISH_LEVELS:
+            raise Exception("Invalid English Level")
+        self.__english_level = value
+
+    @skills.setter
+    def skills(self, value: List[str]):
+        self._skills = self.validate_skill_list(value, "Skills")
+
+    def add_skills(self, value: List[str]):
+        value = self.validate_skill_list(value, "New Skills")
+        combined = set(self._skills) | set(value)
+        self._skills = sorted(list(combined))
+        return self._skills
 
     def to_dict(self):
-        data = {
-            "graduated_id": self.graduated_id,
+        return {
             "personal_cv_url": self.personal_cv_url,
             "skills": self.skills,
             "english_level": self.english_level,
             "works_in_it": self.works_in_it,
         }
 
-        if self.id:
-            data["id"] = self.id
+    @staticmethod
+    def validate_skill_list(value: list[str], field_name: str) -> list[str]:
+        if not isinstance(value, list):
+            raise TypeError(f"{field_name} mus be a list of technologies")
 
-        if self.created_at:
-            data["created_at"] = self.created_at
+        for skill in value:
+            BModel.validate_string(skill, f"{skill} must be a string")
 
-        if self.updated_at:
-            data["updated_at"] = self.updated_at
-
-        return data
+        return value
