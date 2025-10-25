850a417247d617358242ab93922c289b2694bd9a
federico-paganini <perfedefer@gmail.com>
2025-10-24 23:51:42 -0300

    feat: Bucket service, now avatar image correctly saved. Full registration flow implemented.

--- app/services/use_cases/register_user/user_processor.py
+++ app/services/use_cases/register_user/user_processor.py
@@ -1,11 +1,20 @@
 from fastapi import UploadFile
 from typing import Dict, Any, Optional
+from PIL import Image
+from io import BytesIO
 import logging
 
+from app.settings import settings
 from app.domain.models.user import User
 from app.domain.models.cvinfo import CVInfo
+from app.domain.ports.supabase_storage_port import ISupabaseStorage
 from app.infrastructure.database.repositories.user_repository import (
     UserRepository,
+)
+
+from app.services.exceptions.register_user_exceptions import (
+    InvalidFileType,
+    FileTooLarge,
 )
 
 logger = logging.getLogger(__name__)
@@ -15,7 +24,7 @@ class UserProcessor:
     def __init__(
         self,
         user_repo: Optional[UserRepository] = None,
-        bucket_service=None,
+        bucket_service: Optional[ISupabaseStorage] = None,
     ):
         self.user_repo = user_repo or UserRepository()
         self.bucket_service = bucket_service
@@ -25,38 +34,57 @@ class UserProcessor:
         cv_info: Dict[str, Any],
         cv_info_data: CVInfo,
         avatar_img: UploadFile,
-        email: str = "perfedefer@gmail.com",
-        cohort: int = 26,
+        email: str,
+        cohort: int,
+        github: str,
     ):
         if await self.user_repo.user_email_exists(email):
             raise Exception(f"User with email {email} already registered")
 
+        await self._validate_img(avatar_img)
         avatar_img_url = await self._save_avatar_img(avatar_img)
         user_data = {
             "first_name": cv_info["first_name"],
             "last_name": cv_info["last_name"],
             "email": email,
-            "avatar_url": avatar_img_url,
+            "avatar_url": avatar_img_url["file_url"],
+            "avatar_path": avatar_img_url["file_path"],
             "cohort": cohort,
             "cv_info": cv_info_data,
         }
+
+        if github:
+            user_data["github"] = github
 
         new_user = User(**user_data)
         user_id = await self._save_user(new_user)
         return user_id
 
-    async def _save_avatar_img(self, avatar_img: UploadFile) -> str:
-        return "https://via.placeholder.com/150"
+    async def _save_avatar_img(self, avatar_img: UploadFile) -> dict[str, str]:
+        return await self.bucket_service.upload(avatar_img, "avatars")
 
     async def _save_user(self, new_user: User) -> str:
         user_id = await self.user_repo.create(new_user.to_dict())
         return user_id
 
-    async def update_user_external_data(
-        self, user_id: str, github_info_id: Optional[str] = None
-    ) -> bool:
-        data = {}
-        if github_info_id:
-            data["github_info"] = github_info_id
-        update_result = await self.user_repo.update(user_id, data)
-        return update_result
+    async def _validate_img(self, file: UploadFile):
+        MAX_IMG_SIZE_BYTES = settings.max_img_size * 1024 * 1024
+
+        if not file.content_type.startswith("image/"):
+            raise InvalidFileType(f"{file.filename} is not a valid image")
+
+        file.file.seek(0, 2)
+        size = file.file.tell()
+        file.file.seek(0)
+        if size > MAX_IMG_SIZE_BYTES:
+            raise FileTooLarge(
+                f"{file.filename} exceeds {settings.max_img_size:.1f}MB limit"
+            )
+
+        try:
+            img = Image.open(BytesIO(await file.read()))
+            img.verify()
+        except Exception:
+            raise InvalidFileType(f"{file.filename} is not a valid image")
+        finally:
+            await file.seek(0)
