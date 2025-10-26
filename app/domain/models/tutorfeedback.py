4e69d59322bb49413b6f5cde68cd678cc25cc279
federico-paganini <perfedefer@gmail.com>
2025-10-26 16:19:27 -0300

    feat: Tutorfeedback domain entity

--- app/domain/models/tutorfeedback.py
+++ app/domain/models/tutorfeedback.py
@@ -1,17 +1,115 @@
-from typing import Optional, List, Dict, Any, Literal
+from typing import Optional, Literal
+from datetime import datetime
+import uuid
+
+from app.domain.models.bmodel import BModel
 
 
 class TutorFeedback:
+    options = ["poor", "average", "very good", "excellent"]
+
     def __init__(
         self,
-        tutor_id: Optional[str],
+        tutor_id: str,
+        id: Optional[str] = None,
         professional_score: Optional[
-            Literal["poor", "avergae", "very good", "excellent"]
-        ],
-        technical_score: Optional[Literal["poor", "avergae", "very good", "excellent"]],
-        comments: Optional[List[Dict[str, Any]]],
+            Literal["poor", "average", "very good", "excellent"]
+        ] = None,
+        technical_score: Optional[
+            Literal["poor", "average", "very good", "excellent"]
+        ] = None,
+        annotation: Optional[str] = None,
+        created_at: Optional[datetime] = None,
     ):
-        self.tutor_id: Optional[str]
+        self.__id = id or uuid.uuid4()
+        self.__tutor_id = BModel.validate_id(tutor_id)
         self.professional_score = professional_score
         self.technical_score = technical_score
-        self.comments = comments
+        self.annotation = annotation
+        self.created_at = created_at
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def tutor_id(self):
+        return self.__tutor_id
+
+    @property
+    def professional_score(self):
+        return self.__professional_score
+
+    @property
+    def technical_score(self):
+        return self.__technical_score
+
+    @property
+    def annotation(self):
+        return self.__annotation
+
+    @property
+    def created_at(self):
+        return self.__created_at
+
+    @professional_score.setter
+    def professional_score(self, value: Optional[str]):
+        if not value:
+            self.__professional_score = None
+
+        if isinstance(value, str):
+            value = value.lower().strip()
+
+        if value not in self.options:
+            raise ValueError(
+                f"Invalid professional score: {value}. Allowed: {self.options}"
+            )
+
+        self.__professional_score = value.capitalize()
+
+    @technical_score.setter
+    def technical_score(self, value: Optional[str]):
+        if not value:
+            self.__technical_score = None
+
+        if isinstance(value, str):
+            value = value.lower().strip()
+
+        if value not in self.options:
+            raise ValueError(
+                f"Invalid technical score: {value}. Allowed: {self.options}"
+            )
+
+        self.__technical_score = value.capitalize()
+
+    @annotation.setter
+    def annotation(self, value: Optional[str]):
+        if not value:
+            self.__annotation = None
+
+        self.__annotation = BModel.validate_string(value, "annotation")
+
+    @created_at.setter
+    def created_at(self, value: Optional[datetime]):
+        if not value:
+            self.__created_at = datetime.now()
+
+        self.__created_at = BModel.validate_datetime(value, "created_at")
+
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "tutor_id": self.tutor_id,
+            "created_at": self.created_at,
+        }
+
+        if self.technical_score:
+            data["technical_score"] = self.technical_score
+
+        if self.professional_score:
+            data["professional_score"] = self.professional_score
+
+        if self.annotation:
+            data["annotation"] = self.annotation
+
+        return data
