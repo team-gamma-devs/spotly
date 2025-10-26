13afcd8d11a82f8fe2f1c4dc6e4c7e754d7548ee
federico-paganini <perfedefer@gmail.com>
2025-10-26 18:41:31 -0300

    fix: Decorator now checks supabase token.

--- app/api/decorators/jwt_validation.py
+++ app/api/decorators/jwt_validation.py
@@ -2,8 +2,11 @@ from fastapi import Request, HTTPException
 from functools import wraps
 from jose import jwt, JWTError
 from typing import Callable, Any
+import logging
 
 from app.settings import settings
+
+logger = logging.getLogger(__name__)
 
 
 def require_jwt(for_manager: bool = False):
@@ -28,20 +31,28 @@ def require_jwt(for_manager: bool = False):
                 raise HTTPException(status_code=403, detail="Token not provided")
 
             token = auth_header.split(" ")[1]
+            logger.info(f"Token: {token}")
 
             try:
                 payload = jwt.decode(
-                    token, settings.secret_key, algorithms=[settings.algorithm]
+                    token,
+                    settings.secret_key,
+                    algorithms=[settings.algorithm],
+                    audience="authenticated",
                 )
+                logger.info(f"payload: {payload}")
             except JWTError:
                 raise HTTPException(status_code=403, detail="Invalid or expired token")
 
             # Guardar info del usuario en el request
             request.state.user = payload
+            logger.info(f"Request: {request.__dict__}")
 
             # Check de rol admin si se requiere
-            if for_manager and not payload.get("is_admin", False):
-                raise HTTPException(status_code=403, detail="Admin access required")
+            if for_manager:
+                role = payload.get("user_metadata", {}).get("role", "").lower()
+                if role != "manager":
+                    raise HTTPException(status_code=403, detail="Admin access required")
 
             return await func(*args, **kwargs)
 
