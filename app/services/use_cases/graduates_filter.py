8eb8be6903befa58c4c7630dca579ef9d6cc8782
Gabriel Barn <gbrielbbarn@gmail.com>
2025-11-18 06:26:39 -0300

    fix: python indentation problems

--- app/services/use_cases/graduates_filter.py
+++ app/services/use_cases/graduates_filter.py
@@ -171,24 +171,24 @@ class GraduatesFilter:
                     if not feedback.get("annotation")
                 }
 
-                data = {
-                    "id": graduate.id,
-                    "first_name": graduate.first_name,
-                    "last_name": graduate.last_name,
-                    "email": graduate.email,
-                    "cv_url": graduate.cv_info["personal_cv_url"],
-                    "english_level": graduate.cv_info["english_level"],
-                    "avatar_url": graduate.avatar_url,
-                    "cohort": graduate.cohort,
-                    "tech_stack": graduate.cv_info["skills"],
-                    "github_url": graduate.github_url if graduate.github_url else "",
-                    "linkedin_url": graduate.cv_info["linkedin_url"],
-                    "created_at": graduate.created_at,
-                    "updated_at": graduate.updated_at,
-                    "annotations": annotations,
-                    "works_in_it": graduate.cv_info["works_in_it"],
-                    "tutors_feedback": general_feedback,
-                }
-                response.append(data)
+            data = {
+                "id": graduate.id,
+                "first_name": graduate.first_name,
+                "last_name": graduate.last_name,
+                "email": graduate.email,
+                "cv_url": graduate.cv_info["personal_cv_url"],
+                "english_level": graduate.cv_info["english_level"],
+                "avatar_url": graduate.avatar_url,
+                "cohort": graduate.cohort,
+                "tech_stack": graduate.cv_info["skills"],
+                "github_url": graduate.github_url if graduate.github_url else "",
+                "linkedin_url": graduate.cv_info["linkedin_url"],
+                "created_at": graduate.created_at,
+                "updated_at": graduate.updated_at,
+                "annotations": annotations,
+                "works_in_it": graduate.cv_info["works_in_it"],
+                "tutors_feedback": general_feedback,
+            }
+            response.append(data)
 
         return response
